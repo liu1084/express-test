c97feea879f799361968feb51f89f8d6897d5d16
jim liu <liu1084@163.com>
2016-03-23 14:02:21 +0800

    update: 1: add db.config; 2: remove db setup from app and use db config module

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,13 @@
  *
  */
 var express = require('express');
-var bodyParser = require('body-parser');
+var db = require('../common/db/db.config').db;
+db.bind('user');
 var app = express();
-var logger = require('morgan');
-app.use(logger('dev'));
 
+//取得user集合中的所有记录
 app.get('/', function (req, res) {
-	req.db.user.find().toArray(function (error, items) {
+	db.user.find().toArray(function (error, items) {
 		if (error) {
 			throw new Error(error);
 		}
@@ -17,8 +17,18 @@ app.get('/', function (req, res) {
 	});
 });
 
-app.get('/:id', function (req, res) {
-	req.db.user.findById(req.params.id, function (error, item) {
+//根据email和name取得集合中的doc
+app.get('/email/:email/name/:name', function (req, res) {
+	var email = req.params.email;
+	var name = req.params.name;
+	var options = {};
+	if (email) {
+		options.email = email;
+	}
+	if (name) {
+		options.name = name;
+	}
+	db.user.find(options).toArray(function (error, item) {
 		if (error) {
 			throw new Error(error);
 		}
@@ -26,8 +36,9 @@ app.get('/:id', function (req, res) {
 	});
 });
 
+//新建一个doc
 app.post('/', function (req, res) {
-	req.db.user.insert(req.body, function (error, result, next) {
+	db.user.insert(req.body, function (error, result) {
 		if (error) {
 			throw new Error(error);
 		}
@@ -35,9 +46,16 @@ app.post('/', function (req, res) {
 	});
 });
 
-app.put('/:id', function (req, res) {
+//根据email，修改用户的name
+app.put('/', function (req, res) {
+	var email = req.body.email;
+	var options = {};
+	if (email) {
+		options.email = email;
+	}
+
 	var body = req.body;
-	req.db.user.updateById({id: body.id},{name: body.name, email: body.email}, function(error, result){
+	db.user.update(options, {$set: {name: body.name}}, function (error, result) {
 		if (error) {
 			throw new Error(error);
 		}
